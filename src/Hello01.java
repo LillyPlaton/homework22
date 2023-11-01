8ea3d2486c4b3edd49a72deb37c3a250494af449
Liliana Platon <l.platon@web.de>
2023-11-01 17:03:25 +0100

    make adition array

--- src/Hello01.java
+++ src/Hello01.java
@@ -1,7 +1,28 @@
 public class Hello01 {
 
   public static void main(String[] args) {
-    System.out.println("Hello world");
+
+    int[] arr = {1, 4, 6, 9, 15};
+    int value = 9;
+
+    int low = 0;
+    int high = arr.length;
+    int index = -1;
+
+    while (low <= high) {
+      int mid = low + ((high - low) / 2);
+      if (arr[mid] == value) {
+        index = mid;
+        break;
+      } else if (arr[mid] < value) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+    System.out.println(index);
   }
 
 }
+
+
